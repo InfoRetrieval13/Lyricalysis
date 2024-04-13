d6ba0a12b146d30546a2f0c9f327695a7f3201c7
Kiriketsuki <kiriketsuki@gmail.com>
2024-04-13 22:40:24 +0800

    removed need for spotify api key

--- LyricalysisUI/Backend/app.py
+++ LyricalysisUI/Backend/app.py
@@ -5,16 +5,6 @@ from flask_login import LoginManager
 from flask_cors import CORS
 import pandas as pd
 
-import spotify_token as st
-import httpx
-from secret_keys import SP_DC, SP_KEY, CLIENT_SECRET
-from syrics.api import Spotify
-
-data = st.start_session(SP_DC, SP_KEY)
-access_token = data[0]
-expiration_date = data[1]
-sp = Spotify(SP_DC)
-
 app = Flask(__name__)
 login_manager = LoginManager(app)
 
@@ -36,13 +26,6 @@ emotions = list(df['emotion'].unique())
 emotions.sort()
 emotions = [emotion.capitalize() for emotion in emotions]
 intensity = list(df['intensity'].unique())
-
-def refresh_token():
-    global access_token
-    global expiration_date
-    data = st.start_session(SP_DC, SP_KEY)
-    access_token = data[0]
-    expiration_date = data[1]
 
 CORS(app)
 
@@ -73,23 +56,6 @@ def search():
         return {"error": "Invalid JSON data"}, 400
     print(data)
     return {'data': data}
-
-@app.route('/get_details/<track_id>', methods=['GET'])
-async def get_details(track_id):
-    refresh_token()
-    endpoint = f"https://api.spotify.com/v1/tracks/{track_id}"
-    headers = {
-        "Authorization": f"Bearer {access_token}"
-    }
-    async with httpx.AsyncClient() as client:
-        response = await client.get(endpoint, headers=headers)
-    data = response.json()
-    url = data["external_urls"]["spotify"]
-    name = data["name"]
-    preview_url = data["preview_url"]
-    image = data["album"]["images"][0]["url"]
-    # print(url, name, preview_url, image)
-    return {"url": url, "name": name, "preview_url": preview_url, "image": image}
 
 @app.route('/send_search', methods=['POST'])
 def query_parser():
